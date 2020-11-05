72acf7b5933c297a2a00101df22487f460ef207b
Dakota Fulp <dkfulp@coastal.edu>
2020-11-05 18:14:26 -0500

    Working on zfp resize bug

--- array/zfparray1.h
+++ array/zfparray1.h
@@ -106,12 +106,32 @@ public:
   // array dimensions
   size_t size_x() const { return nx; }
 
+  /**
   // resize the array (all previously stored data will be lost)
   void resize(size_t nx, bool clear = true)
   {
     this->nx = nx;
     store.resize(nx, clear);
     cache.clear();
+  }
+  **/
+
+  // resize the array (all previously stored data will be lost)
+  void resize(size_t nx, bool clear = true, bool save = false)
+  {
+    if (save){
+      value_type* tmp = new value_type[nx]();
+      get(tmp);
+      this->nx = nx;
+      store.resize(nx, clear);
+      cache.clear();
+      set(tmp);
+      delete [] tmp;
+    } else {
+      this->nx = nx;
+      store.resize(nx, clear);
+      cache.clear();
+    }
   }
 
   // rate in bits per value
