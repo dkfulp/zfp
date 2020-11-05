c218cbcdb39eb5438560f2731d70b5ce024207d5
Dakota Fulp <dkfulp@coastal.edu>
2020-11-05 17:45:55 -0500

    Fixing Resize bug

--- array/zfparray1.h
+++ array/zfparray1.h
@@ -112,11 +112,11 @@ public:
     printf("zfparray1.h resize function called\n");
     if (save){
       value_type* tmp = new value_type[nx]();
-      get(tmp);
+      //get(tmp);
       this->nx = nx;
       store.resize(nx, clear);
       cache.clear();
-      set(tmp);
+      //set(tmp);
       delete [] tmp;
     } else {
       this->nx = nx;
