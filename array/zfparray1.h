45fe19b526239b483b58de018da1eb2899535bcc
Dakota Fulp <dkfulp@coastal.edu>
2020-11-19 21:38:41 -0500

    Working on resize function

--- array/zfparray1.h
+++ array/zfparray1.h
@@ -122,17 +122,16 @@ public:
     cout << "zfparray1.h resize function called" << endl;
     if (save){
       this->data_holder = std::malloc(nx * sizeof(value_type));
-
-      get(static_cast<value_type>this->data_holder );
+      get(static_cast<value_type>(this->data_holder));
       cout << "Temp Array:" << endl;
       for (int i = 0; i < nx; i++){
-          std::cout << static_cast<value_type>this->data_holder[i] << " " << std::endl;
+          std::cout << static_cast<value_type>(this->data_holder)[i] << " " << std::endl;
       }
       this->nx = nx;
       store.resize(nx, clear);
       cache.clear();
-      set(static_cast<value_type>this->data_holder);
-      std::free(this->data_holder);
+      set(static_cast<value_type>(this->data_holder));
+      std::free((this->data_holder));
 
     }else {
       this->nx = nx;
